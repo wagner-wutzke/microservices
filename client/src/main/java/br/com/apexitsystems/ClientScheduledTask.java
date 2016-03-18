3d33688fa8d0b3c5862eaf29fa04dbccc6abfd19
Wagner Otto Wutzke <wagner.wutzke@nordeck.net>
2016-03-18 19:54:58 -0300

    Removed REST Service from client and added a new Scheduled component, which calls the REST service provider every 10 secs.

---
+++ client/src/main/java/br/com/apexitsystems/ClientScheduledTask.java
@@ -0,0 +1,27 @@
+package br.com.apexitsystems;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ClientScheduledTask {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ClientScheduledTask.class);
+	
+	@Autowired
+	@LoadBalanced
+    private RestTemplate restTemplate;
+	
+	@Scheduled(fixedDelay=10000)
+	public void callService() {
+		String url = "http://SERVICE" + "/service";
+		String response = restTemplate.getForObject(url, String.class);
+		LOG.info("This is the response from the service: [{}]", response);
+	}
+
+}

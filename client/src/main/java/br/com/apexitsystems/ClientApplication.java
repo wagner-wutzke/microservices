565c5ddf86066b895ec62cad9736bd205d183dca
Wagner Otto Wutzke <wagner.wutzke@nordeck.net>
2016-03-19 15:03:19 -0300

    Added forgotten @EnableSchedling annotation to ClientApplication

--- client/src/main/java/br/com/apexitsystems/ClientApplication.java
+++ client/src/main/java/br/com/apexitsystems/ClientApplication.java
@@ -2,8 +2,10 @@ package br.com.apexitsystems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClientApplication {
 
 	public static void main(String[] args) {
